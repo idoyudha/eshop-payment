d9bab97f6cea4e8ef9ab862f3f5066c6f0c852b5
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-23 17:28:02 +0700

    feat: mysql connector deps, update aws region value, and add jakarta validation

--- src/main/java/com/github/eshop/payment/application/dto/CreatePaymentRequest.java
+++ src/main/java/com/github/eshop/payment/application/dto/CreatePaymentRequest.java
@@ -1,5 +1,6 @@
 package com.github.eshop.payment.application.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -7,10 +8,10 @@ import java.util.UUID;
 
 @Data
 public class CreatePaymentRequest {
-//    @NotNull(message = "Order ID is required")
+    @NotNull(message = "Order ID is required")
     private UUID orderId;
 
-//    @NotNull(message = "Image is required")
+    @NotNull(message = "Image is required")
     private MultipartFile image;
 
     private String note;
