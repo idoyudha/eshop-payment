d55e78bdbe9f9e5d5f538584587df762bdc582e2
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-23 14:24:59 +0700

    refactor: order to payment

--- src/main/java/com/github/eshop/payment/App.java
+++ src/main/java/com/github/eshop/payment/App.java
@@ -1,4 +1,4 @@
-package com.github.eshop.order;
+package com.github.eshop.payment;
 
 /**
  * Hello world!
--- src/test/java/com/github/eshop/payment/AppTest.java
+++ src/test/java/com/github/eshop/payment/AppTest.java
@@ -1,4 +1,4 @@
-package com.github.eshop.order;
+package com.github.eshop.payment;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
