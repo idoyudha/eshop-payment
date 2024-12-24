98195aeb1b27dc033e7c5b95a3b37e31a6b914c2
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-24 10:57:35 +0700

    refactor: add json alias for order_id

--- src/main/java/com/github/eshop/payment/application/dto/CreatePaymentRequest.java
+++ src/main/java/com/github/eshop/payment/application/dto/CreatePaymentRequest.java
@@ -1,5 +1,6 @@
 package com.github.eshop.payment.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +10,7 @@ import java.util.UUID;
 @Data
 public class CreatePaymentRequest {
     @NotNull(message = "Order ID is required")
+    @JsonAlias("order_id")
     private UUID orderId;
 
     @NotNull(message = "Image is required")
