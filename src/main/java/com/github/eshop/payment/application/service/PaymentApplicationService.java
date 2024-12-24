c18a22cc6467d2132ba1cac6ec7bace1f4eba563
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-24 13:45:42 +0700

    feat: s3Service

--- src/main/java/com/github/eshop/payment/application/service/PaymentApplicationService.java
+++ src/main/java/com/github/eshop/payment/application/service/PaymentApplicationService.java
@@ -2,14 +2,17 @@ package com.github.eshop.payment.application.service;
 
 import com.github.eshop.payment.application.dto.CreatePaymentRequest;
 import com.github.eshop.payment.application.dto.PaymentResponse;
+import com.github.eshop.payment.application.exception.PaymentException;
 import com.github.eshop.payment.domain.entity.Payment;
 import com.github.eshop.payment.domain.entity.PaymentStatus;
 import com.github.eshop.payment.domain.service.PaymentService;
+import com.github.eshop.payment.infrastructure.s3.S3Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -18,27 +21,33 @@ import java.util.UUID;
 @Slf4j
 public class PaymentApplicationService {
     private final PaymentService paymentService;
+    private final S3Service s3Service;
 
     @Transactional
     public PaymentResponse createPayment(CreatePaymentRequest request) {
-        log.info("received payment request: {}", request);
-        // TODO: upload image to S3
-        String imageUrl = "";
+        try {
+            log.info("received payment request: {}", request);
+            String imageUrl = s3Service.uploadImage(request.getImage());
 
-        Payment payment = Payment.builder()
-                .id(UUID.randomUUID())
-                .orderId(request.getOrderId())
-                .imageUrl(imageUrl)
-                .status(PaymentStatus.PENDING)
-                .note(request.getNote())
-                .createdAt(LocalDateTime.now())
-                .updatedAt(LocalDateTime.now())
-                .build();
+            Payment payment = Payment.builder()
+                    .id(UUID.randomUUID())
+                    .orderId(request.getOrderId())
+                    .imageUrl(imageUrl)
+                    .status(PaymentStatus.PENDING)
+                    .note(request.getNote())
+                    .createdAt(LocalDateTime.now())
+                    .updatedAt(LocalDateTime.now())
+                    .build();
 
-        log.info("created payment entity: {}", payment);
-        Payment savedPayment = paymentService.createPayment(payment);
+            log.info("created payment entity: {}", payment);
+            Payment savedPayment = paymentService.createPayment(payment);
 
-        return paymentEntityToPaymentResponse(savedPayment);
+            return paymentEntityToPaymentResponse(savedPayment);
+        } catch (IOException e) {
+            log.error("failed to upload payment image: {}", e.getMessage());
+            throw new PaymentException("failed to upload payment image: " + e.getMessage());
+        }
+
     }
 
     public PaymentResponse getPayment(UUID id) {
--- src/main/java/com/github/eshop/payment/infrastructure/s3/S3Service.java
+++ src/main/java/com/github/eshop/payment/infrastructure/s3/S3Service.java
@@ -1,4 +1,56 @@
 package com.github.eshop.payment.infrastructure.s3;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${aws.cloudfront.domain-name}")
+    private String cloudfrontDomain;
+
+    public String uploadImage(MultipartFile file) throws IOException {
+        String filename = generateFileName(file);
+        String key = "payments/" + filename;
+
+        try {
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(file.getContentType())
+                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+            return String.format("https://%s/%s", cloudfrontDomain, key);
+        } catch (Exception e) {
+            log.error("Error uploading file to S3: {}", e.getMessage());
+            throw new IOException("Failed to upload file to S3", e);
+        }
+    }
+
+    // format: UUID-YYYYMMDDHHMMSS.extension
+    private String generateFileName(MultipartFile file) {
+        String originalFileName = file.getOriginalFilename();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        return UUID.randomUUID().toString() + "-" + timestamp + extension;
+    }
 }
