af69eb82e8e93a2ba17b355267d5f78aaaeba3d2
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-25 21:38:48 +0700

    feat: standarize api response

--- src/main/java/com/github/eshop/payment/controller/rest/PaymentController.java
+++ src/main/java/com/github/eshop/payment/controller/rest/PaymentController.java
@@ -2,6 +2,7 @@ package com.github.eshop.payment.controller.rest;
 
 import java.util.UUID;
 
+import com.github.eshop.payment.application.dto.ApiResponse;
 import com.github.eshop.payment.application.dto.UpdatePaymentStatusRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -22,20 +23,35 @@ public class PaymentController {
     private final PaymentApplicationService paymentApplicationService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<PaymentResponse> createPayment(@ModelAttribute CreatePaymentRequest request) {
-        PaymentResponse response = paymentApplicationService.createPayment(request);
+    public ResponseEntity<ApiResponse<PaymentResponse>> createPayment(@ModelAttribute CreatePaymentRequest request) {
+        PaymentResponse payment = paymentApplicationService.createPayment(request);
+        ApiResponse<PaymentResponse> response = ApiResponse.<PaymentResponse>builder()
+                .code(HttpStatus.CREATED.value())
+                .data(payment)
+                .message("payment created successfully")
+                .build();
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PaymentResponse> getPayment(@PathVariable UUID id) {
-        PaymentResponse response = paymentApplicationService.getPayment(id);
+    public ResponseEntity<ApiResponse<PaymentResponse>> getPayment(@PathVariable UUID id) {
+        PaymentResponse payment = paymentApplicationService.getPayment(id);
+        ApiResponse<PaymentResponse> response = ApiResponse.<PaymentResponse>builder()
+                .code(HttpStatus.OK.value())
+                .data(payment)
+                .message("payment retrieved successfully")
+                .build();
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping()
-    public ResponseEntity<PaymentResponse> updatePaymentStatus(@Valid @RequestBody UpdatePaymentStatusRequest request) {
-        PaymentResponse response = paymentApplicationService.updatePaymentStatus(request);
+    public ResponseEntity<ApiResponse<PaymentResponse>> updatePaymentStatus(@Valid @RequestBody UpdatePaymentStatusRequest request) {
+        PaymentResponse payment = paymentApplicationService.updatePaymentStatus(request);
+        ApiResponse<PaymentResponse> response = ApiResponse.<PaymentResponse>builder()
+                .code(HttpStatus.OK.value())
+                .data(payment)
+                .message("payment status updated successfully")
+                .build();
         return ResponseEntity.ok(response);
     }
 }
