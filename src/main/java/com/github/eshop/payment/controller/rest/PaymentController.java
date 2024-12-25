05d85e4316e4e86e171bac0fc6912f835e936b01
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-25 18:47:38 +0700

    refactore: remove api in controller mapping

--- src/main/java/com/github/eshop/payment/controller/rest/PaymentController.java
+++ src/main/java/com/github/eshop/payment/controller/rest/PaymentController.java
@@ -1,30 +1,35 @@
 package com.github.eshop.payment.controller.rest;
 
-import com.github.eshop.payment.application.dto.CreatePaymentRequest;
-import com.github.eshop.payment.application.dto.PaymentResponse;
-import com.github.eshop.payment.application.service.PaymentApplicationService;
-import lombok.RequiredArgsConstructor;
+import java.util.UUID;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
+import com.github.eshop.payment.application.dto.CreatePaymentRequest;
+import com.github.eshop.payment.application.dto.PaymentResponse;
+import com.github.eshop.payment.application.service.PaymentApplicationService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("api/v1/payments")
+@RequestMapping("v1/payments")
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentApplicationService paymentApplicationService;
 
-    // TODO: add cognito middleware
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<PaymentResponse> createPayment(@ModelAttribute CreatePaymentRequest request) {
         PaymentResponse response = paymentApplicationService.createPayment(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
-    // TODO: add cognito middleware
     @GetMapping("/{id}")
     public ResponseEntity<PaymentResponse> getPayment(@PathVariable UUID id) {
         PaymentResponse response = paymentApplicationService.getPayment(id);
