4087c1eca241c68386e5eb637ce7f53ad5bff704
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-02 20:58:33 +0700

    feat: add config for kafka producer

--- src/main/java/com/github/eshop/payment/infrastructure/kafka/PaymentKafkaProducer.java
+++ src/main/java/com/github/eshop/payment/infrastructure/kafka/PaymentKafkaProducer.java
@@ -30,7 +30,7 @@ public class PaymentKafkaProducer {
                     .build();
             String message = objectMapper.writeValueAsString(event);
             kafkaTemplate.send(paymentUpdatedTopic, payment.getId().toString(), message);
-            log.info("published payment updated event: {}", message);
+            log.info("published {} event with message: {}", paymentUpdatedTopic, message);
         } catch (Exception e) {
             log.error("error publishing payment updated event: {}", e.getMessage());
         }
--- src/main/java/com/github/eshop/payment/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/github/eshop/payment/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.github.eshop.payment.infrastructure.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,10 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        TODO: need to put in .env for allowed origins
+        // TODO: need to put in .env for allowed origins
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
